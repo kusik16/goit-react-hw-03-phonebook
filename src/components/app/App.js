665ceeb356248a944274383a97ee6303827c5199
kusik16 <slobodyanuck@gmail.com>
2022-09-03 15:53:24 +0300

    commit

---
+++ src/components/app/App.js
@@ -0,0 +1,106 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactForm from '../contactForm/ContactForm';
+import Filter from '../filter/Filter';
+import ContactList from '../contactList/ContactList';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [],
+      filter: '',
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('contacts') !== null) {
+      return this.setState({
+        contacts: JSON.parse(localStorage.getItem('contacts')),
+      });
+    }
+  }
+
+  onFilter = e => {
+    this.setState(() => {
+      return {
+        filter: e.target.value,
+      };
+    });
+  };
+
+  onAddContact = (name, number) => {
+    if (
+      this.state.contacts.filter(contact => contact.name === name).length >= 1
+    ) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    if (
+      this.state.contacts.filter(contact => contact.number === number).length >=
+      1
+    ) {
+      alert(`${number} is already in contacts`);
+      return;
+    }
+
+    const newUser = {
+      name,
+      number,
+      id: nanoid(),
+    };
+
+    localStorage.setItem(
+      'contacts',
+      JSON.stringify([newUser, ...this.state.contacts])
+    );
+
+    this.setState(() => {
+      return {
+        contacts: [newUser, ...this.state.contacts],
+      };
+    });
+  };
+
+  onDeleteContact = id => {
+    localStorage.setItem(
+      'contacts',
+      JSON.stringify(this.state.contacts.filter(contact => contact.id !== id))
+    );
+
+    this.setState(() => {
+      return {
+        contacts: this.state.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const filteredContacts = !filter
+      ? contacts
+      : contacts.filter(
+          contact =>
+            contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+            (contact.number + '').includes(filter)
+        );
+
+    return (
+      <div>
+        <h1 className="title">Phonebook</h1>
+        <ContactForm onAddContact={this.onAddContact} />
+        <h2 className="title">Contacts</h2>
+        <Filter onFilter={this.onFilter} />
+        <ContactList
+          filteredContacts={filteredContacts}
+          onDeleteContact={this.onDeleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/contactForm/ContactForm.js
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './contactForm.css';
+
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      number: '',
+    };
+  }
+
+  render() {
+    const { name, number } = this.state;
+    const { onAddContact } = this.props;
+
+    return (
+      <div className="form">
+        <label htmlFor="name">Name</label>
+        <input
+          className="form__input"
+          onChange={e =>
+            this.setState(() => {
+              return {
+                name: e.target.value,
+              };
+            })
+          }
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <label htmlFor="number">Number</label>
+        <input
+          className="form__input"
+          onChange={e =>
+            this.setState(() => {
+              return {
+                number: e.target.value,
+              };
+            })
+          }
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <button
+          className="form__button"
+          onClick={() => onAddContact(name, number)}
+        >
+          Add contact
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ContactForm;
+
+ContactForm.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+};
